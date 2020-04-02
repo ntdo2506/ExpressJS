b676425e7e9c5f5cc715fbdf2d5b523ab1c11837
ThanhDo <ntdo.13cdt1@gmail.com>
2020-04-02 21:07:09 +0700

    Lesson 3: Query parameters

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+const users = [
+    {id: 1, name: 'Do'},
+    {id: 2, name: 'Thao'}
+];
+
 app.get('/', (req, res)=>{
     res.render('index',{
         name: 'AAA'
@@ -14,11 +19,16 @@ app.get('/', (req, res)=>{
 
 app.get('/users', (req, res)=>{
     res.render('users/index', {
-        users:[
-            {id: 1, name: 'Do'},
-            {id: 2, name: 'Thao'}
-        ]
-    })
+        users: users
+    });
+});
+
+app.get('/users/search', (req, res) => {
+    let q = req.query.q;
+    let matchedUser = users.filter((user) => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
+    res.render('users/index', {
+        users: matchedUser
+    });
 });
 
 app.listen(port, ()=>{
